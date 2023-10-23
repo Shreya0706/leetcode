8b57c6e260a118d8bc4cd836d80bbbfa906450e2
Shreya Gupta <80683920+Shreya0706@users.noreply.github.com>
2023-10-23 23:52:34 +0530

    Time: 6 ms (65.36%), Space: 43.8 MB (39.38%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,38 +1,23 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int i=0, j=0, count = 0, maxCount = 0;
+         int n = s.length();
+        int maxLength = 0;
+        Set<Character> charSet = new HashSet<>();
+        int left = 0;
         
-        // to store all english characters
-        final boolean[] visited = new boolean[127];
-        final int n = s.length();
-        while (i < n) {
-            
-            // find first repeating character
-            while (i < n && !visited[s.charAt(i)]) {
-                count++;
-                visited[s.charAt(i)] = true;
-                i++;
+        for (int right = 0; right < n; right++) {
+            if (!charSet.contains(s.charAt(right))) {
+                charSet.add(s.charAt(right));
+                maxLength = Math.max(maxLength, right - left + 1);
+            } else {
+                while (charSet.contains(s.charAt(right))) {
+                    charSet.remove(s.charAt(left));
+                    left++;
+                }
+                charSet.add(s.charAt(right));
             }
-            
-            // update max count
-            maxCount = Math.max(maxCount, count);
-            
-            // checks if reached end
-            if (i == n) break;
-            
-            // find the earlier occurance of i and remove all characters before that in the string
-            while (s.charAt(j) != s.charAt(i)) {
-                visited[s.charAt(j)] = false;
-                j++;
-                count--;
-            }
-            
-            // move both the pointers to continue the max substring lookup
-            j++;
-            i++;
         }
         
-        return maxCount;
+        return maxLength;
     }
-    
 }
