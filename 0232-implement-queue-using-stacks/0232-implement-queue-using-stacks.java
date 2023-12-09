fdb47c945acab78e807029b7fafd1b7f2f371d27
Shreya Gupta <80683920+Shreya0706@users.noreply.github.com>
2023-12-09 16:51:47 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (32.40%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -0,0 +1,59 @@
+class MyQueue {
+
+     Stack<Integer> first;
+    Stack<Integer> second;
+    public MyQueue() 
+    { 
+      first = new Stack<>();
+      second = new Stack<>();
+    }
+    public void push(int x)
+    {
+      first.push(x);
+    }
+    
+    // first pop it from first stack and push into second stack, 
+    // then pop the element from second stack and again pop 
+    // it from second stack and push in first stack 
+    public int pop() 
+    {
+         while(!first.isEmpty())
+         second.push(first.pop());
+         
+         int removed = second.pop();
+
+         while(!second.isEmpty())
+         first.push(second.pop());
+         
+         return removed;
+    }
+    // same thing in peek as pop
+    public int peek() 
+    {
+         while(! first.isEmpty())
+         second.push(first.pop());
+         
+         int peeked= second.peek();
+
+         while(!second.isEmpty())
+         first.push(second.pop());
+         
+         return peeked;
+    }
+    
+    public boolean empty()
+    {
+      return first.isEmpty() && second.isEmpty();
+    }
+        
+    
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
